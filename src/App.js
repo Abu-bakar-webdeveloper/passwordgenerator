bca90d674f6c553d8f6dc27b5f228e1eed6224d5
abubkar <abubakaraw8@gmail.com>
2023-09-12 14:43:15 +0500

    passwordgenerator app using react  hooks start

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+import {useState, useCallback} from 'react'
 import './App.css';
 
 function App() {
+  const [length, setLenght] = useState(8);
+  const [numberAllowed, setnumberAllowed] = useState(false);
+  const [charAllowed, setCharAllowed] = useState(false);
+  const [password, setPassword] = useState("");
+
+  const passwordGenerator = useCallback(() => {
+    let pass = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+    if (numberAllowed) str += "0123456789";
+    if (charAllowed) str += "&*.{}[],=-";
+
+    for (let i = 1; i <= Array.length; i++) {
+      let char = Math.floor(Math.random() * str.length + 1);
+      pass = str.charAt(char);
+    }
+
+    setPassword(pass);
+  }, [length, numberAllowed, charAllowed, setPassword]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <>
+      <div className='w-full max-w-md mx-auto shadow-md
+       rounded-lg px-4 my-8 text-orange-500 bg-gray-500
+       '>
+        test
+      </div>
+      </>
+  )
 }
 
 export default App;
